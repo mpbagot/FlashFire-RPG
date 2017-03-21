bf0a4a6517a137239a63adff6b2721845d035762
Mitchell Bagot <packof14eggies@gmail.com>
2017-03-21 19:58:55 +1100

    21/3/17 - Add new area descriptions and effect functions

--- effects.py
+++ effects.py
@@ -6,14 +6,32 @@ pygame.init()
 
 sfx = pygame.mixer.Channel(0)
 
-def printf(text, nline=True):
+def play_intro(filename):
+    for line in open(filename).read().split('\n'):
+        if line:
+            if line.split()[0] == 'play_music':
+                play_music(line.split()[1])
+                continue
+            elif line.split()[0] == 'play':
+                play_sound(line.split()[1])
+                continue
+            if line in ('-','/'):
+                print()
+                if line == '/':
+                    sleep(1)
+                continue
+            printf(str(line),pause = 0.03)
+    sleep(5)
+    print('\n'*30)
+
+def printf(text, nline=True,pause=0.015):
   '''
   Scroll a given string instead of instant printing
   '''
   for char in text:
     sys.stdout.write(char)
     sys.stdout.flush()
-    sleep(0.015)
+    sleep(pause)
   if nline:
     sys.stdout.write('\n')
 

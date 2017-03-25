0f2ab345b7b449ab88b1fe90d401dc4b8a5fb975
Mitchell Bagot <packof14eggies@gmail.com>
2017-03-25 20:56:49 +1100

    25/3/17 - Add story quest towns

--- world.py
+++ world.py
@@ -8,8 +8,12 @@ class World:
         self.diff = difficulty
         self.lod = lod
 
+        self.init_pos = pos
+
         self.is_loading = False
         self.details = None
+
+        self.used_names = []
 
         #Generate an empty array
         self.chunk_array = [[0 for b in range(x//10)] for a in range(y//10)]
@@ -49,10 +53,15 @@ class World:
         for a in range(10):
             for b in range(10):
                 # Iterate the x and y of the array and create nodes to fill it
+                # If the node we are making is the very first in the whole game
+                if [x*10+a, y*10+b] == self.init_pos:
+                    # Then generate the village of Little Ivywood
+                    chunk.array[b][a] = self.make_node(chunk, a, b, diff, lod, True)
+                    continue
                 chunk.array[b][a] = self.make_node(chunk, a, b, diff, lod)
         return chunk
 
-    def make_node(self, chunk, x, y, dif, lod):
+    def make_node(self, chunk, x, y, dif, lod, start=False):
         '''
         Generate a new random area nodes
         '''
@@ -91,6 +100,7 @@ class World:
 
         #Generate randomly difficult enemies based on difficulty
         enemies = []
+        # Remove the or true after testing
         if random.randint(1,9)//dc[dif] == 1:
             for a in range(random.randint(1, int(dc[dif]))):
                 enemies.append(Enemy(dc[dif], [x,y]))
@@ -113,12 +123,65 @@ class World:
         # spawn npc's randomly and populate this list.
         npc = []
         pos = (x, y)
+        # If you're not in a city, then create some NPC's
         if typ != 'city' and random.randint(0, 5)//dc[dif] > 0:
             for a in range(random.randint(0, 4)):
                 npc.append(NPC(pos))
 
+        # If this is the very first node of the game
+        if start:
+            # Create some basic enemies
+            enemies = [Enemy(dc[dif], [x,y]) for a in range(4)]
+            for i in range(len(enemies)):
+                # Iterate and make the enemies super weak, to ensure the player doesn't die
+                enemies[i].hp = 2+random.randint(0, 5)
+                enemies[i].attack = 3+random.randint(0, 3)
+            # Create thee story-based NPCs
+            npc = [NPC(pos, True) for a in range(3)]
+            town = Quest_Town(True, True, True, True, enemies, npc, [])
+            # Set the town name
+            town.town_name = 'Little Ivywood'
+            return town
+
+        # Possibly spawn a quest town for the storyline if they haven't all spawned
+        if random.randint(0,25000) == 777 and len(self.used_names) < open('towns.txt').read().split('\n'):
+            # Spawn lots of enemies
+            enemies = [Enemy(dc[dif], [x,y]) for a in range(random.randint(3, 5))]
+            # If it's an unlucky town, spawn a Demon boss there
+            if random.randint(0, 2) != 1:
+                enemies.append(Demon(dc[dif], [x,y]))
+            # Create several people
+            npc = [NPC(pos, True) for a in range(random.randint(2, 4))]
+            # Create the node
+            node = Quest_Town(hasNorth, hasSouth, hasEast, hasWest, enemies, npc, self.used_names)
+            # Add the towns name to the used names
+            self.used_names.append(node.town_name)
+            return node
+
         # Return the generated Area_Node object
         return Area_Node(hasNorth, hasSouth, hasEast, hasWest, enemies, typ, npc)
+
+    def spawn_endgame(self, pos):
+        '''
+        Spawn the endgame lair of Strevras Rane
+        '''
+        chunk_pos = [a//10 for a in pos]
+        # Get a random nearby chunk coordinates
+        x = random.randint(chunk_pos[0]-5, chunk_pos[0]+5)*10
+        y = random.randint(chunk_pos[1]-5, chunk_pos[1]+5)*10
+        # Get a random node within the chunk
+        x2 = random.randint(0, 9)
+        y2 = random.randint(0, 9)
+        # Keep doing the above until the randomly selected node is not the current one
+        while self.get_node(x+x2, y+y2) == self.get_node(*pos):
+            x = random.randint(chunk_pos[0]-5, chunk_pos[0]+5)*10
+            y = random.randint(chunk_pos[1]-5, chunk_pos[1]+5)*10
+            x2 = random.randint(0, 9)
+            y2 = random.randint(0, 9)
+        # Set the selected node as an end area
+        self.chunk_array[y//10][x//10].array[y2][x2] = End_Area()
+        # Return an xy coordinate list of the spawned node
+        return [x+x2, y+y2]
 
     def set_node(self, pos, **kwargs):
         '''
@@ -256,7 +319,7 @@ class Area_Node:
         if len(self.npc) == 1:
             # If there's one person nearby then tell the player that
             string += '\nA strange person stands nearby. They appear harmless, but possibly afraid.'
-        elif len(self.npc) > 3:
+        elif len(self.npc) > 1:
             # If there's a few then tell the player that there is a group
             string += '\nA group of people cower nearby. They seem greatly afraid of something.'
 
@@ -308,3 +371,100 @@ class Area_Node:
 class Null_Node(Area_Node):
     def __init__(self):
         super().__init__(False,False,False,False,[],'Hills',[])
+
+class Quest_Town(Area_Node):
+    def __init__(self,n,s,e,w,enemies, npc, used_names=[], town_name=None):
+        super().__init__(n,s,e,w,enemies,'Town',npc)
+        self._npc = list(self.npc)
+        self.npc = []
+        self.is_cleared = False
+        if town_name:
+            self.town_name = town_name
+        else:
+            names = open('towns.txt').read().split('\n')
+            names = [a for a in names if a not in used_names]
+            if len(names)-1 == 0:
+                self.town_name = 'Random name'
+                return
+            self.town_name = names[random.randint(0, len(names)-1)]
+
+    def get_description(self):
+        '''
+        Get the description of the town
+        '''
+        if len(self.enemies) == 0:
+            self.is_cleared = True
+        if self.is_cleared:
+            self.npc = list(self._npc)
+        string = ''
+        d2 = {'North':self.hasNorth, 'South':self.hasSouth, 'East':self.hasEast, 'West':self.hasWest}
+        # Get the passage description based on the movable directions
+        if all([d2[a] for a in d2]):
+            string += '\nThere are roads in all directions.'
+        else:
+            for a in d2:
+                if d2[a]:
+                    # If the direction is travelable (?) then add the notification to the description string
+                    if string and string[-1] not in ('.', '\n'):
+                        string += ', another leads to the {}.'.format(a)
+                        continue
+                    string += '\nA road leads to the {}'.format(a)
+        string += '\n'
+
+        if len(self.npc) == 1:
+            # If there's one person nearby then tell the player that
+            string += '\nA single person stands nearby. They seem to be waiting for someone.'
+        elif len(self.npc) > 1:
+            # If there's a few then tell the player that there is a group
+            string += '\nA group of people mill about nearby. They seem greatly bored, but relieved.'
+
+        if len(self.enemies) > 3:
+            # If there are lots of enemies then tell the player that they are in trouble!
+            string += '\nYou are surrounded by enemies!'
+        else:
+            # Otherwise just state the number of enemies.
+            string += ('\nThere '+('are {} enemies' if len(self.enemies) != 1 else 'is {} enemy')+' nearby.').format(len(self.enemies))
+
+        if self.is_cleared:
+            # If the town has been cleared out then tell the user
+            return 'People bustle around the small town of {}, relieved that you helped free them from Rane\'s monsters'.format(self.town_name)+string
+        else:
+            # Otherwise narrate the level of devastation being wreaked by the monsters
+            return 'You are in {}. Monsters run around the town, destroying the houses of the people.'.format(self.town_name)+string
+
+    def __str__(self):
+        '''
+        Get the save string used in the save file.
+        '''
+        x = super().__str__()
+        x += '/'+self.town_name+'/'+str(int(self.is_cleared))
+        return x
+
+    @staticmethod
+    def get_by_string(string):
+        '''
+        Generate a node by pre-saved data
+        '''
+        #Pull passageway data
+        n,s,e,w = (bool(int(string[0])), bool(int(string[1])), bool(int(string[2])), bool(int(string[3])))
+        string = string.split('/')[1:]
+        #Get the enemies
+        enemies = [Enemy.get_by_string(a) for a in eval(string[0])]
+        #Get the node area type
+        typ = 'Town'
+        npc = [NPC.get_by_string(a) for a in eval(string[2])]
+        node = Quest_Town(n, s, e, w, enemies, npc, town_name=string[3])
+        node.is_cleared = bool(int(string[4]))
+        return node
+
+class End_Area(Area_Node):
+    def __init__(self):
+        super().__init__(True, True, True, True, [Rane()], 'lair', [])
+
+    def get_description(self):
+        '''
+        Return the description of the area
+        '''
+        if self.enemies:
+            return 'A large cavern opens before you as the Demon Eyes fuse together, and begin to emanate a dark energy.\nThe warlock, Strevras Rane stands at the far end, facing you.'
+        return 'You are in a large rocky cavern.\nStrevras Rane has been defeated, his energy no longer radiates from here.'

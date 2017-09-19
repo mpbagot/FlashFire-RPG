d9530f308e098d8bd0fb4215575b8dc9b38712c2
Mitchell Bagot <packof14eggies@gmail.com>
2017-09-19 13:12:04 +1000

    Remove Now Unneccessary Documentation

--- line_counter.py
+++ line_counter.py
@@ -2,11 +2,14 @@ import os
 
 total = 0
 comments = 0
+files = []
 for filename in os.listdir():
     if filename.endswith('.py') and filename != 'line_counter.py':
         for line in open(filename).read().split('\n'):
             total += 1 if line != '' and not line.strip().startswith('#') else 0
             comments += 1 if line.strip().startswith('#') else 0
+        files.append(filename)
 
+print("Files Scanned: \n - {}".format('\n - '.join(files)))
 print("Lines of Code: {}\nComments: {}".format(total, comments))
 print("Code to Comment Ratio: {} : 1".format(round(total/comments, 2)))
